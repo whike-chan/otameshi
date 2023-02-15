b4236c367d6733c80b28646b4a8846455b5be781
whike <10iridescent.symphogear01@gmail.com>
2023-02-15 16:44:08 +0900

    update

--- docs/sample/dtree/dtree.js
+++ docs/sample/dtree/dtree.js
@@ -51,29 +51,6 @@ treeJson = d3.json("./data.json", function(error, treeData) {
             //         return text;
             //     },
             callbacks: {
-                textRenderer: function(name, extra, textClass) {
-                    let text = '';
-                    // 分配割合
-                    if (extra && extra.share) {
-                        text += `<div class="card-share">${extra.share}</div>`;
-                    }
-                    text += '<div class="card-bg">';
-
-                    // 画像
-                    text += '<div class="card-image">';
-                    if (extra && extra.image) {
-                        text += `<img src="./img/${extra.image}.png" alt="" width="64" height="64">`
-                    } else {
-                        text += `<img src="./img/young-man.png" alt="" width="64" height="64">`
-                    }
-                    text += '</div>';
-
-                    // 続柄
-                    text += `<span class="card-relation">${name}</span>`;
-
-                    text += '</div>';
-                    return text;
-                },
                 nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
                     // This callback is optional but can be used to customize the
                     // node element using HTML.
@@ -85,6 +62,14 @@ treeJson = d3.json("./data.json", function(error, treeData) {
                     node += textRenderer(name, extra, textClass);
                     node += '</div>';
                     return node;
+                },
+                textRenderer: function(name, extra, textClass) {
+                    // THis callback is optinal but can be used to customize
+                    // how the text is rendered without having to rewrite the entire node
+                    // from screatch.
+                    if (extra && extra.nickname)
+                        name = name + " (" + extra.nickname + ")";
+                    return "<p align='center' class='" + textClass + "'>" + name + "</p>";
                 }
             }
         }
