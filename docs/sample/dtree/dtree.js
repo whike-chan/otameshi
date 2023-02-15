3a0c976f380a3a02d4863fcc0b068212ad1864a9
whike <10iridescent.symphogear01@gmail.com>
2023-02-15 15:38:25 +0900

    Update dtree.js

--- docs/sample/dtree/dtree.js
+++ docs/sample/dtree/dtree.js
@@ -52,24 +52,21 @@ treeJson = d3.json("./data.json", function(error, treeData) {
             //     },
             // },
             callbacks: {
-                nodeClick: function(name, extra) {
-                  console.log(name);
-                },
                 textRenderer: function(name, extra, textClass) {
                     // THis callback is optinal but can be used to customize
-                  // how the text is rendered without having to rewrite the entire node
-                  // from screatch.
+                    // how the text is rendered without having to rewrite the entire node
+                    // from screatch.
                     if (extra && extra.nickname)
                       name = name + " (" + extra.nickname + ")";
-                    return "<p align='center' class='" + textClass + "'>" + name + "</p>";
+                    return "<p align='center' class='" + textClass + "'><img src='./img/young-man.png' alt='' width='64' height='64'>" + name + "</p>";
                 },
                 nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
                   // This callback is optional but can be used to customize the
                   // node element using HTML.
                   let node = '';
                   node += '<div ';
-                  node += 'style="height:100%;width:100%;" ';
-                  node += 'class="' + nodeClass + '" ';
+                //   node += 'style="height:100%;width:100%;" ';
+                  node += 'class="card-wrapper" ';
                   node += 'id="node' + id + '">\n';
                   node += textRenderer(name, extra, textClass);
                   node += '</div>';
