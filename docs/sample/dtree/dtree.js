d086f41180349137bd1a10952163bb4a5a7ab91a
whike <10iridescent.symphogear01@gmail.com>
2023-02-15 13:44:42 +0900

    Add dtree

---
+++ docs/sample/dtree/dtree.js
@@ -0,0 +1,56 @@
+treeJson = d3.json("./data.json", function(error, treeData) {
+    dTree.init(treeData,
+        {
+            target: "#graph",
+            debug: true,
+            hideMarriageNodes: true,
+            marriageNodeSize: 5,
+            height: 800,
+            width: 1200,
+            nodeWidth: 84,
+            callbacks: {
+                nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
+                    let node = '';
+                    node += '<div class="card-wrapper';
+                    if (extra && extra.inheritee) {
+                        // 分配対象者
+                        node += ' is-inheritee';
+                    } else if(extra && extra.deceased) {
+                        // 故人
+                        node += ' is-deceased';
+                    }
+                    if (extra && extra.you) {
+                        node += ' is-you';
+                    }
+                    node += '" style="background-color: gray;">';
+                    node += textRenderer(name, extra, textClass);
+                    node += '</div>';
+                    return node;
+                },
+                textRenderer: function(name, extra, textClass) {
+                    let text = '';
+                    // 分配割合
+                    if (extra && extra.share) {
+                        text += `<div class="card-share">${extra.share}</div>`;
+                    }
+                    text += '<div class="card-bg">';
+
+                    // 画像
+                    text += '<div class="card-image">';
+                    if (extra && extra.image) {
+                        text += `<img src="./img/${extra.image}.png" alt="" width="64" height="64">`
+                    } else {
+                        text += `<img src="./img/young-man.png" alt="" width="64" height="64">`
+                    }
+                    text += '</div>';
+
+                    // 続柄
+                    text += `<span class="card-relation">${name}</span>`;
+
+                    text += '</div>';
+                    return text;
+                },
+            },
+        }
+    );
+});
