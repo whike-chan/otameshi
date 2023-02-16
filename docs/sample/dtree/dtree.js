3f54a126c6699fdccdbfe345764f51e94880e876
whike <10iridescent.symphogear01@gmail.com>
2023-02-16 16:52:27 +0900

    update dtree

--- docs/sample/dtree/dtree.js
+++ docs/sample/dtree/dtree.js
@@ -7,54 +7,12 @@ treeJson = d3.json("./data.json", function(error, treeData) {
             marriageNodeSize: 5,
             height: 800,
             width: 1200,
-            nodeWidth: 84,
-            // callbacks: {
-            //     nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
-            //         let node = '';
-            //         node += '<div class="card-wrapper';
-            //         if (extra && extra.inheritee) {
-            //             // 分配対象者
-            //             node += ' is-inheritee';
-            //         } else if(extra && extra.deceased) {
-            //             // 故人
-            //             node += ' is-deceased';
-            //         }
-            //         if (extra && extra.you) {
-            //             node += ' is-you';
-            //         }
-            //         node += '">';
-            //         node += textRenderer(name, extra, textClass);
-            //         node += '</div>';
-            //         return node;
-            //     },
-            //     textRenderer: function(name, extra, textClass) {
-            //         let text = '';
-            //         // 分配割合
-            //         if (extra && extra.share) {
-            //             text += `<div class="card-share">${extra.share}</div>`;
-            //         }
-            //         text += '<div class="card-bg">';
-
-            //         // 画像
-            //         text += '<div class="card-image">';
-            //         if (extra && extra.image) {
-            //             text += `<img src="./img/${extra.image}.png" alt="" width="64" height="64">`
-            //         } else {
-            //             text += `<img src="./img/young-man.png" alt="" width="64" height="64">`
-            //         }
-            //         text += '</div>';
-
-            //         // 続柄
-            //         text += `<span class="card-relation">${name}</span>`;
-
-            //         text += '</div>';
-            //         return text;
-            //     },
+            nodeWidth: 92,
             callbacks: {
                 // ラッパー
                 nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
                     let node = '';
-                    node += '<div width="82" height="106" ';
+                    node += '<div style="width:100%;height:100%;" ';
                     node += 'class="card-wrapper ';
                     if (extra && extra.inheritee) {
                         // 分配対象者
@@ -80,10 +38,10 @@ treeJson = d3.json("./data.json", function(error, treeData) {
                     if (extra && extra.share) {
                         text += `${extra.share}`;
                     }
-                    text += `</span></div>`;
+                    text += `</span><br><span class="card-share-deco"></span></div>`;
 
                     // 画像+続柄ラッパー
-                    text += '<div width="72" height="96" class="card-person">';
+                    text += '<div class="card-person">';
 
                     // あなた
                     text += '<div class="card-you"><span class="card-you-tag">';
